fceebd670b25e320c6d40f9d996663d3c3ee019b
Takuya <takuya@asial.co.jp>
2023-11-10 12:30:40 +0900

    fix to support monaca localkit

--- src/App.vue
+++ src/App.vue
@@ -4,54 +4,56 @@
       <div class="center">{{ title }}</div>
     </v-ons-toolbar>
 
-    <v-ons-tabbar position="auto"
+    <v-ons-tabbar
+      position="auto"
       :tabs="tabs"
       :visible="true"
-      :index.sync="activeIndex"
+      v-model:index="activeIndex"
     >
     </v-ons-tabbar>
   </v-ons-page>
 </template>
 
 <script>
-  import settingsPage from './Settings.vue';
-  import homePage from './Home.vue';
-  import newsPage from './News.vue';
+import { markRaw } from 'vue';
+import settingsPage from "./Settings.vue";
+import homePage from "./Home.vue";
+import newsPage from "./News.vue";
 
-  export default {
-    data() {
-      return {
-        activeIndex: 0,
-        tabs: [
-	  {
-	    icon: this.md() ? null : 'ion-ios-home',
-	    label: 'Home',
-	    page: homePage
-	  },
-	  {
-	    icon: this.md() ? null : 'ion-ios-notifications',
-	    label: 'News',
-	    page: newsPage,
-	    badge: 7
-	  },
-	  {
-	    icon: this.md() ? null : 'ion-ios-settings',
-	    label: 'Settings',
-	    page: settingsPage
-	  }
-        ]
-      };
+export default {
+  data() {
+    return {
+      activeIndex: 0,
+      tabs: [
+        {
+          icon: this.md() ? null : "ion-ios-home",
+          label: "Home",
+          page: markRaw(homePage),
+        },
+        {
+          icon: this.md() ? null : "ion-ios-notifications",
+          label: "News",
+          page: markRaw(newsPage),
+          badge: 7,
+        },
+        {
+          icon: this.md() ? null : "ion-ios-settings",
+          label: "Settings",
+          page: markRaw(settingsPage),
+        },
+      ],
+    };
+  },
+  methods: {
+    md() {
+      return this.$ons.platform.isAndroid();
     },
-    methods: {
-      md() {
-        return this.$ons.platform.isAndroid();
-      }
+  },
+  computed: {
+    title() {
+      return this.tabs[this.activeIndex].label;
     },
-    computed: {
-      title() {
-        return this.tabs[this.activeIndex].label;
-      }
-    },
-    components: { homePage, settingsPage, newsPage }
-  }
+  },
+  components: { homePage, settingsPage, newsPage },
+};
 </script>
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import path from 'path';
 import vue from '@vitejs/plugin-vue';
+import legacy from '@vitejs/plugin-legacy';
 
 const SRC_DIR = path.resolve(__dirname, './src');
 const BUILD_DIR = path.resolve(__dirname, './www');
@@ -9,6 +10,9 @@ const HOST = process.env.MONACA_SERVER_HOST || argvs.host || '0.0.0.0';
 export default {
   plugins: [
     vue(),
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+    })
   ],
   root: SRC_DIR,
   base: '',
